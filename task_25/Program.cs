991c914c3a631c7d077c35ae4c7e630eaef15053
Irina <kopytova.irina@inbox.ru>
2023-01-16 19:43:11 +0300

    HW_4

---
+++ task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (без использования Math.Pow)
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int  N(int A, int B)
+{
+    int n = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        n = n * A;
+    }
+    return n;
+}
+
+Console.WriteLine("Введите число A: ");
+int nA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int nB = Convert.ToInt32(Console.ReadLine());
+int n = N(nA, nB);
+Console.WriteLine(n);
---
+++ task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int  K(int number)
+{
+    int k = 0;
+    int n = 0;
+    while (number > 0)
+    {
+        k = number - number % 10 ;
+        n = n + (number - k);
+        number = number/10;
+    }
+    return n;
+}
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = K(N);
+Console.WriteLine(n);
